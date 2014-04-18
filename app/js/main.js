8fa250cb03773b3e25ed24272caaa0bab351993b
Jim Wharton <jamesawharton@gmail.com>
2014-04-18 07:51:55 -0600

    both histograms with a sep

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,15 @@ var yearHist = d3.select('#content').append("svg").attr({
   height:150
 });
 
+var sep = d3.select("#content").append("div").attr({
+  class:'vert-seperator'
+});
+
+var areaHist = d3.select('#content').append("svg").attr({
+  width:300,
+  height:150
+});
+
 d3.json("js/mtbs-fires.json", function(collection) {
 
   var transform = d3.geo.transform({point: projectPoint});
@@ -68,6 +77,19 @@ d3.json("js/mtbs-fires.json", function(collection) {
       // y:0,
       width:9,
       height: function(d) {return d.numFires * .1;},
+      fill: "orange"
+    });
+
+  areaHist.selectAll("rect")
+    .data(firesByYear)
+    .enter()
+    .append("rect")
+    .attr({
+      x:function(d, i) { return i * 10;},
+      y:function(d, i) { return 150 - (d.area * .00001);},
+      // y:0,
+      width:9,
+      height: function(d) {return d.area * .00001;},
       fill: "orange"
     });
 
